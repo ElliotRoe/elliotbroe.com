ae6c2d758e55c2ac01f7ede5841f94e333a05fc4
Roe, Elliot Benjamin <eroe7@gatech.edu>
2023-11-28 10:22:26 -0500

    feat: updated small card styling

--- components/small-card.tsx
+++ components/small-card.tsx
@@ -4,7 +4,7 @@ import { InfoCircledIcon } from "@radix-ui/react-icons";
 import Image from "next/image";
 import Link from "next/link";
 import { FC, useState } from "react";
-import { useInterval } from "usehooks-ts";
+import { useElementSize } from "usehooks-ts";
 
 type SmallCardProps = {
   experience: Experience;
@@ -12,33 +12,40 @@ type SmallCardProps = {
 };
 
 export const SmallCard: FC<SmallCardProps> = ({ experience, className }) => {
+  const [squareRef, { height }] = useElementSize();
+  console.log(height);
+
   return (
     <div
-      className={`mt-4 card flex flex-row justify-center items-center border rounded-md ${className}`}
+      className={`mt-4 card flex flex-row justify-between items-center border rounded-md ${className}`}
     >
-      <Link
-        className="px-2 h-full flex items-center hover:bg-muted transition-colors duration-200 ease-in-out"
-        href={experience.slug}
-      >
-        <InfoCircledIcon />
-      </Link>
-      <Separator orientation="vertical" className="h-full" />
-      <div className="flex items-center justify-center w-2/3 h-full p-5">
+      <div className="h-full flex flex-row">
+        <Link
+          className="px-2 h-full flex items-center hover:bg-muted transition-colors duration-200 ease-in-out"
+          href={experience.slug}
+        >
+          <InfoCircledIcon />
+        </Link>
+        <Separator orientation="vertical" className="h-full" />
+      </div>
+      <div className="flex items-center justify-center h-full p-5">
         <div className="text-md font-bold leading-none">
           {experience.position}
         </div>
       </div>
-      <div className="relative h-full w-1/3">
+      <div className={`h-full flex flex-row w-20`} ref={squareRef}>
         <Separator orientation="vertical" className="h-full" />
-        <Image
-          src={experience.logoPath ?? "/images/placeholder.png"}
-          alt="BxCoding"
-          fill
-          className="rounded-md rounded-l-none"
-          style={{
-            objectFit: "contain",
-          }}
-        />
+        <div className="relative h-full w-full">
+          <Image
+            src={experience.logoPath ?? "/images/placeholder.png"}
+            alt="BxCoding"
+            fill
+            className="rounded-md rounded-l-none"
+            style={{
+              objectFit: "cover",
+            }}
+          />
+        </div>
       </div>
     </div>
   );
